2435909d0e145b0df064069b67ea850e3b273214
Jose Ferney Perez Camacho <josefepe29@gmail.com>
2023-09-28 02:36:44 -0500

    Manejo de Router y modificacion a funcionamiento de lista de tareas

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const express = require ('express')
-const app = express()
-const host = 'localhost'
-const tareasModule = require('./script');
+const express = require('express');
+const app = express();
+const port = 3000;
+const vista = require('./list-view-router');
+const edicion = require('./list-edit-router');
 
-const tareas = tareasModule.tareas
-
-const port = 8080
-
-app.get('/', (req, res) => {
-    console.log(tareas)
-  res.send(JSON.stringify(tareas))
-})
+// Middleware para permitir solicitudes JSON
+app.use(express.json());
+app.use('/edicion', edicion)
+app.use('/vista',vista)
 
 app.listen(port, () => {
-    console.log(`Servidor activo en ${port}`)
-})
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
